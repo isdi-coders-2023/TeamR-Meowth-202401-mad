9fd1a05a51ea772ae9c0ea40cbf0484b54d766ee
apradanab <apradanab@gmail.com>
2024-04-01 15:16:03 +0200

    Add paginator

--- src/app/feature/home/home.component.ts
+++ src/app/feature/home/home.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { PaginatorComponent } from '../paginator/paginator.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [PaginatorComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
