492b5bc2b91d509af5c92a946e7bd4bec51e37ed
FerreiroAlberto <homelandz@gmail.com>
2024-04-01 12:47:48 +0200

    Add RepoService and Data Model

---
+++ src/app/core/model/model.ts
@@ -0,0 +1,19 @@
+export interface CardList {
+  cards: MagicCard[];
+}
+
+export interface MagicCard {
+  name: string;
+  manaCost: string;
+  cmc: number;
+  colors: string[];
+  rarity: string;
+  setName: string;
+  text: string;
+  artist: string;
+  number: string;
+  imageUrl: string;
+  isFavorite: boolean;
+  types: string[];
+  flavor: string;
+}
