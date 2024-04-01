4d3d5b88c46929476484e58beb010313185a7b7a
apradanab <apradanab@gmail.com>
2024-04-01 19:46:25 +0200

    Add filter component

--- src/app/core/store/store.service.ts
+++ src/app/core/store/store.service.ts
@@ -10,15 +10,19 @@ export class StoreService {
   private state = new BehaviorSubject<MagicCard[]>([]);
   public state$ = this.state.asObservable();
   currentPage = 1;
+  currentColor = '';
+  currentType = '';
   constructor(private repo: RepoService) {
     this.loadData();
   }
 
-  loadData( page: number = this.currentPage) {
+  loadData( page: number = this.currentPage, color: string = this.currentColor, type: string = this.currentType) {
 
-    this.repo.getData(page).subscribe({
+    this.repo.getData(page, color, type).subscribe({
       next: (data) => {this.state.next(data.cards.filter((data) => data.imageUrl));
-      this.currentPage = page},
+      this.currentPage = page
+      this.currentColor = color
+      this.currentType = type},
       error: (dataError) => console.log('Es un error.' + dataError),
     });
   }
---
+++ src/app/feature/filter/filter.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import { StoreService } from '../../core/store/store.service';
+
+@Component({
+  selector: 'app-filter',
+  standalone: true,
+  imports: [],
+  templateUrl: './filter.component.html',
+  styleUrl: './filter.component.css'
+})
+export class FilterComponent {
+  currentColor: string = '';
+  currentType: string = '';
+constructor(private store: StoreService) {}
+changeColor( color: string ) {
+  this.store.loadData(this.store.currentPage, color)
+  this.currentColor = color;
+}
+changeType( type: string ) {
+  this.store.loadData(this.store.currentPage, this.store.currentColor, type)
+  this.currentType = type;
+}
+resetFilter()  {
+  this.currentColor = '';
+  this.currentType = '';
+  this.store.loadData(this.store.currentPage, this.currentColor, this.currentType)
+}
+}
