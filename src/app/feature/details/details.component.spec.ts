452bf8062991b464a65d5e6950c6a35eedbd767f
apradanab <apradanab@gmail.com>
2024-04-03 18:40:07 +0200

    Upload details component with testing

--- src/app/feature/details/details.component.spec.ts
+++ src/app/feature/details/details.component.spec.ts
@@ -1,6 +1,10 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { DetailsComponent } from './details.component';
+import  DetailsComponent  from './details.component';
+import { MagicCard } from '../../core/model/model';
+import { By } from '@angular/platform-browser';
+import { provideHttpClient } from '@angular/common/http';
+import { provideRouter } from '@angular/router';
 
 describe('DetailsComponent', () => {
   let component: DetailsComponent;
@@ -8,7 +12,8 @@ describe('DetailsComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [DetailsComponent]
+      imports: [DetailsComponent],
+      providers: [provideHttpClient(),provideRouter([])]
     })
     .compileComponents();
 
@@ -18,6 +23,16 @@ describe('DetailsComponent', () => {
   });
 
   it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+  it('should display card info correctly', () => {
+    const testCard = {
+      name: 'testName'
+    } as MagicCard
+    component.card = testCard
+    fixture.detectChanges();
+    const element = fixture.debugElement.query(By.css('h2')).nativeElement
+    expect(element.textContent).toContain('testName')
     expect(component).toBeTruthy();
   });
 });
