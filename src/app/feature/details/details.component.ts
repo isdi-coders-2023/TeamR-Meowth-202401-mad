f753cd7711465bcd07aa09f29f9ed264cd39b055
fran hernandez <franciscohernandezlopez5@gmail.com>
2024-04-04 13:55:20 +0200

    Add test repo and modificed css

--- src/app/feature/details/details.component.ts
+++ src/app/feature/details/details.component.ts
@@ -5,17 +5,24 @@ import { RouterModule } from '@angular/router';
 import { HeaderComponent } from '../../shared/header/header.component';
 import { FooterComponent } from '../../shared/footer/footer.component';
 import { MenuComponent } from '../../shared/menu/menu.component';
+import { PaginatorComponent } from '../paginator/paginator.component';
 
 @Component({
   selector: 'app-details',
   standalone: true,
-  imports: [RouterModule, HeaderComponent, FooterComponent, MenuComponent ],
   templateUrl: './details.component.html',
-  styleUrl: './details.component.css'
+  styleUrl: './details.component.css',
+  imports: [
+    RouterModule,
+    HeaderComponent,
+    FooterComponent,
+    MenuComponent,
+    PaginatorComponent,
+  ],
 })
 export default class DetailsComponent {
-  card : MagicCard = {} as MagicCard;
-  constructor( private store: StoreService) {
+  card: MagicCard = {} as MagicCard;
+  constructor(private store: StoreService) {
     this.store.getSelectedCard().subscribe((card) => (this.card = card));
   }
 }
