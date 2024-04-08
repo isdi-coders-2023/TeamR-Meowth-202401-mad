0985d5f26ae7ca677f8dc09b0253126b9ff07833
fran hernandez <franciscohernandezlopez5@gmail.com>
2024-04-08 09:44:13 +0200

    Update testint

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,32 +3,32 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   {
     path: 'home',
-    title: 'Home',
+    title: 'ProjectMeowth:Home',
     loadComponent: () => import('./feature/home/home.component'),
   },
   {
     path: 'details',
-    title: 'details',
+    title: 'ProjectMeowth:Details',
     loadComponent: () => import('./feature/details/details.component'),
   },
   {
     path: 'error',
-    title: 'error',
+    title: 'ProjectMeowth:Error',
     loadComponent: () => import('./feature/error/error.component'),
   },
   {
     path: 'favorite',
-    title: 'favorite',
+    title: 'ProjectMeowth:Favorite',
     loadComponent: () => import('./feature/favorite/favorite.component'),
   },
   {
     path: 'create',
-    title: 'create',
+    title: 'ProjectMeowth:Create',
     loadComponent: () => import('./feature/createcard/createcard.component'),
   },
   {
     path: 'edit',
-    title: 'edit',
+    title: 'ProjectMeowth:Edit',
     loadComponent: () =>
       import('./feature/editcard/editcard/editcard.component'),
   },
